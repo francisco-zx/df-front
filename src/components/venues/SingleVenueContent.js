6f421160c8fdd51970e435608010f4920ede6cc9
Francisco Aboy <franciscoaboy@gmail.com>
2018-05-28 17:40:55 -0300

    responsive single venues

--- src/components/venues/SingleVenueContent.js
+++ src/components/venues/SingleVenueContent.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { StyleSheet, css } from 'aphrodite';
 
 import SingleVenueDetails from './SingleVenueDetails';
 import SingleVenueEventsSlider from './SingleVenueEventsSlider';
@@ -8,41 +9,61 @@ export default class SingleVenueContent extends Component {
     return (
       <div>
         <div className='container'>
-          <article style={style.details}>
-            <h3 style={style.subTitle}>ACERCA DE</h3>
-            <p style={style.description}>
-            Ubicado en el barrio de Colegiales. El teatro Vorterix es un hito local. Consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
-            </p>
-            <h3 style={style.subTitle}>UBICACION</h3>
-            <p style={style.description}>
-              Avenida Federico Lacroze 3455,
-              Ciudad de Buenos Aires
-            </p>
-            <h3 style={style.subTitle}>COMO LLEGAR</h3>
-            <p style={style.description}>
-              Colectivo: 19  21 – 39 – 42 – 44 – 63 – 65 – 76 – 87 – 93 – 108 – 112 – 140 – 151 – 168 – 176 – 184
-              Tren: Línea Mitre Ramal Retiro-José León Suárez est. Colegiales (a 7 cuadras) / Línea Urquiza (Metrovías) est. Federico Lacroze (a 6 cuadras)
-              Subte: Línea B est. Federico Lacroze (a 6 cuadras)
-            </p>
-          </article>
-          <article style={style.flyer}>
-            <SingleVenueDetails />
-          </article>
+          <div className={css(style.content)}>
+            <article className={css(style.details)}>
+              <h3 className={css(style.subTitle)}>ACERCA DE</h3>
+              <p className={css(style.description)}>
+              Ubicado en el barrio de Colegiales. El teatro Vorterix es un hito local. Consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+              </p>
+              <h3 className={css(style.subTitle)}>UBICACION</h3>
+              <p className={css(style.description)}>
+                Avenida Federico Lacroze 3455,
+                Ciudad de Buenos Aires
+              </p>
+              <h3 className={css(style.subTitle)}>COMO LLEGAR</h3>
+              <p className={css(style.description)}>
+                Colectivo: 19  21 – 39 – 42 – 44 – 63 – 65 – 76 – 87 – 93 – 108 – 112 – 140 – 151 – 168 – 176 – 184
+                Tren: Línea Mitre Ramal Retiro-José León Suárez est. Colegiales (a 7 cuadras) / Línea Urquiza (Metrovías) est. Federico Lacroze (a 6 cuadras)
+                Subte: Línea B est. Federico Lacroze (a 6 cuadras)
+              </p>
+            </article>
+            <article className={css(style.flyer)}>
+              <SingleVenueDetails />
+            </article>
+          </div>
         </div>
         <SingleVenueEventsSlider />
+
+
       </div>
 
     );
   }
 }
-const style = {
+const style = StyleSheet.create({
+  content: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    "@media(max-width: 1024px)": {
+      flexDirection: 'column',
+    }
+  },
   details: {
-    width: '65%',
-    display: 'inline-block'
+    display: 'flex',
+    flex: 4,
+    flexDirection: 'column',
+    "@media(max-width: 1024px)": {
+      flex: 1,
+    }
   },
   flyer: {
-    width: '35%',
-    display: 'inline-block'
+    display: 'flex',
+    flex: 2,
+    height: 'auto',
+    "@media(max-width: 1024px)": {
+      flex: 1,
+    }
   },
   title: {
     fontSize: '3rem',
@@ -62,4 +83,4 @@ const style = {
     paddingRight: '2rem',
     marginBottom: '3rem',
   }
-}
+})
